5e05649e70e379b715c9c5c699bc4699d2cc9ab9
makawneranjit <68535617+makawneranjit@users.noreply.github.com>
2023-09-09 19:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (7.25%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,7 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        /* Approach: Apply binary search 
+        /* Approach1 : Apply binary search matrix considering it as an 1D array
+        Time : O (log m*n) | Space: O (1) */
+
+        int m = matrix.length;
+        int n = matrix[0].length; 
+
+        int start = 0;
+        int end = m*n - 1; 
+
+        while(start <= end) {
+            int mid = start + (end-start)/2; 
+            int element = matrix[mid/n][mid%n];
+
+            if(element == target)
+                return true;
+
+            else if (element < target)
+                start  = mid+1;
+
+            else
+                end = mid-1; 
+        }
+
+        return false; 
+
+//____________________________________________________________________________________________________________
+        /* Approach 2: Apply binary search 
         
         1. Apply binary seach on col to find the row.
         2. Apply binary search on row to find the target
@@ -9,32 +35,31 @@ class Solution {
         Time: O(long m + log n) | Space : (1) */
 
         //traverse each row
-        int m = matrix.length;
-        int n = matrix[0].length; 
 
-        for(int i=0; i<m; i++) {
+        // for(int i=0; i<m; i++) {
 
-            //on each row apply binary search
-            if(target >= matrix[i][0] && target <= matrix[i][n-1]) {
-                int start = 0;
-                int end = n-1;
-                
-                while(start <= end) {
-                    int mid = start + (end-start)/2;
+        //     //on each row apply binary search
+        //     if(target >= matrix[i][0] && target <= matrix[i][n-1]) {
+        //         int start = 0;
+        //         int end = n-1;
 
-                    if(matrix[i][mid] == target)
-                        return true;
+        //         while(start <= end) {
+        //             int mid = start + (end-start)/2;
 
-                    else if(matrix[i][mid] > target)
-                        end = mid-1; 
+        //             if(matrix[i][mid] == target)
+        //                 return true;
 
-                    else
-                     start = mid+1; 
-                }  
-            }
-        }
+        //             else if(matrix[i][mid] > target)
+        //                 end = mid-1; 
 
-        return false; 
+        //             else
+        //              start = mid+1; 
+        //         }  
+        //     }
+        // }
+
+        // return false; 
+
     }
 
 }
